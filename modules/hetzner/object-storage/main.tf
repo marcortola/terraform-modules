ff9efae3c8c73bd68d16c8fbf80e6131e3d98e11
marcortola <15958009+marcortola@users.noreply.github.com>
2026-02-19 13:17:48 +0100

    Prevent bucket_prefix from triggering force-replacement in object-storage module

--- modules/hetzner/object-storage/main.tf
+++ modules/hetzner/object-storage/main.tf
@@ -10,4 +10,9 @@ resource "minio_s3_bucket" "buckets" {
   for_each = var.buckets
   bucket        = each.value.name
   acl           = each.value.acl
+
+  lifecycle {
+    prevent_destroy = true
+    ignore_changes  = [bucket_prefix]
+  }
 }
